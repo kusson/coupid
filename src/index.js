a5e3a8d983ea76ab196fc77e3cb934a583b5be92
Kusson <kussonlks@gmail.com>
2023-01-24 10:57:46 +1100

    Fixing routing issues with Hashrouter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import '../src/App.css';
 import App from "./App"
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter basename= {"/${process.env.PUBLIC_URL"}>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
